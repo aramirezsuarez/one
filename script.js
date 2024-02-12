22ae8b671d4b36e27d5cdc4f62892534454fc188
aramirezsuarez <aramirezsu@unal.edu.co>
2024-02-11 20:06:44 -0500

    correccion de errores

--- script.js
+++ script.js
@@ -1,94 +1,90 @@
-var botonEncriptar = document.querySelector(".boton_encriptar");
-var botonDesencriptar = document.querySelector(".boton_dencriptar");
-var munieco = document.querySelector(".logo_b");
-var contenedor = document.querySelector(".mensaje");
-var resultado = document.querySelector(".texto-resultado");
+const botonEncriptar = document.querySelector('.boton_encriptar')
+const botonDesencriptar = document.querySelector('.boton_dencriptar')
+const botonCopiar = document.querySelector('.boton_copiar')
+const munieco = document.querySelector('.logo_b')
+const contenedor = document.querySelector('.mensaje')
+const resultado = document.querySelector('.texto-resultado')
 
-botonEncriptar.onclick = encriptar;
-botonDesencriptar.onclick = desencriptar;
+botonEncriptar.onclick = encriptar
+botonDesencriptar.onclick = desencriptar
+botonCopiar.onclick = copiarTexto
 
-function encriptar(){
-    ocultarAdelante();
-    var cajatexto = recuperarTexto()
-    resultado.textContent = encriptarTexto(cajatexto);
+function encriptar() {
+  ocultarAdelante()
+  let cajatexto = recuperarTexto()
+  resultado.textContent = encriptarTexto(cajatexto)
 }
 
-function desencriptar(){
-    ocultarAdelante();
-    var cajatexto = recuperarTexto()
-    resultado.textContent = desencriptarTexto(cajatexto);
+function desencriptar() {
+  ocultarAdelante()
+  let cajatexto = recuperarTexto()
+  resultado.textContent = desencriptarTexto(cajatexto)
 }
 
-function recuperarTexto(){
-    var cajatexto = document.querySelector(".cajatexto")
-    return cajatexto.value
+function recuperarTexto() {
+  let cajatexto = document.querySelector('.espacio_texto')
+  return cajatexto.value
 }
 
-function ocultarAdelante(){
-    munieco.classList.add("ocultar");
-    contenedor.classList.add("ocultar");
+function ocultarAdelante() {
+  munieco.classList.add('ocultar')
+  contenedor.classList.add('ocultar')
 }
 
-function encriptarTexto(mensaje){
-    var texto = mensaje;
-    var textoFinal = "";
+function encriptarTexto(mensaje) {
+  let texto = mensaje
+  let textoFinal = ''
 
-    for(var i = 0; i < texto.length; i++){
-        if(texto[i] == "a"){
-            textoFinal = textoFinal + "ai"
-        }
-        else if(texto[i] == "e"){
-            textoFinal = textoFinal + "enter"
-        }
-        else if(texto[i] == "i"){
-            textoFinal = textoFinal + "imes"
-        }
-        else if(texto[i] == "o"){
-            textoFinal = textoFinal + "ober"
-        }
-        else if(texto[i] == "u"){
-            textoFinal = textoFinal + "ufat"
-        }
-        else{
-            textoFinal = textoFinal + texto[i]
-        }
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i] == 'a') {
+      textoFinal = textoFinal + 'ai'
+    } else if (texto[i] == 'e') {
+      textoFinal = textoFinal + 'enter'
+    } else if (texto[i] == 'i') {
+      textoFinal = textoFinal + 'imes'
+    } else if (texto[i] == 'o') {
+      textoFinal = textoFinal + 'ober'
+    } else if (texto[i] == 'u') {
+      textoFinal = textoFinal + 'ufat'
+    } else {
+      textoFinal = textoFinal + texto[i]
     }
-    return textoFinal;
-
+  }
+  return textoFinal
 }
 
-function desencriptarTexto(mensaje){
-    var texto = mensaje;
-    var textoFinal = "";
+function desencriptarTexto(mensaje) {
+  let texto = mensaje
+  let textoFinal = ''
 
-    for(var i = 0; i < texto.length; i++){
-        if(texto[i] == "a"){
-            textoFinal = textoFinal + "a"
-            i = i+1;
-        }
-        else if(texto[i] == "e"){
-            textoFinal = textoFinal + "e"
-            i = i+4;
-        }
-        else if(texto[i] == "i"){
-            textoFinal = textoFinal + "i"
-            i = i+3;
-        }
-        else if(texto[i] == "o"){
-            textoFinal = textoFinal + "o"
-            i = i+3;
-        }
-        
-        else if(texto[i] == "u"){
-            textoFinal = textoFinal + "u"
-            i = i+3;
-        }
-        else{
-            textoFinal = textoFinal + texto[i];
-        }
-        
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i] == 'a') {
+      textoFinal = textoFinal + 'a'
+      i = i + 1
+    } else if (texto[i] == 'e') {
+      textoFinal = textoFinal + 'e'
+      i = i + 4
+    } else if (texto[i] == 'i') {
+      textoFinal = textoFinal + 'i'
+      i = i + 3
+    } else if (texto[i] == 'o') {
+      textoFinal = textoFinal + 'o'
+      i = i + 3
+    } else if (texto[i] == 'u') {
+      textoFinal = textoFinal + 'u'
+      i = i + 3
+    } else {
+      textoFinal = textoFinal + texto[i]
     }
+  }
 
-    return textoFinal;
+  return textoFinal
+}
 
+function copiarTexto() {
+  const respuesta = resultado.textContent
+  navigator.clipboard
+    .writeText(respuesta)
+    .then(() => console.log(`Se ha copiado el texto ${respuesta}`))
+    .catch((err) => console.log(err))
 }
